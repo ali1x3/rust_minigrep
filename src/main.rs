606a658a68056adfc5ec62685476f3252f698107
ali1x3 <avilaalex371@gmail.com>
2025-10-19 19:24:46 +0800

    added search functionality

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,44 @@
-use std::{env, fs};
+use std::{env, error::Error, fs, process};
+use minigrep::search;
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Self, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enought Arguements");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+
+    fn print_args(self) -> Self {
+        println!("Searching for {} in {}", self.query, self.file_path);
+        self
+    }
+
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_args(&args);
+    let config = Config::new(&args)
+        .map(|s| s.print_args()).unwrap_or_else(|_err| {
+            println!("Error with parsing");
+            process::exit(1);
+        });
 
-    println!("Searching for {query} in {file_path}");
-
-    let file_contents = fs::read_to_string(file_path);
-
-    match file_contents {
-        Ok(contents) => println!("With text: \n {contents}"),
-        Err(e) => println!("Error: {e}"),
-    }
+    let _ = run(config);
 }
 
-fn parse_args(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
-    (query, file_path)
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+    Ok(())
 }
