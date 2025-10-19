9325cfbbc8657fb8e6d5325a8c4135e4c0e3931e
ali1x3 <avilaalex371@gmail.com>
2025-10-19 15:13:12 +0800

    added parsing of args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let (query, file_path) = parse_args(&args);
+
+    println!("Searching for {query} in {file_path}");
+
+    let file_contents = fs::read_to_string(file_path);
+
+    match file_contents {
+        Ok(contents) => println!("With text: \n {contents}"),
+        Err(e) => println!("Error: {e}"),
+    }
+}
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
